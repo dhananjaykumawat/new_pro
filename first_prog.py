cb4798f485c1d8faae49db5707b824c44a3d858b
dhananjay <dhananjay.kumawat99@gmail.com>
2020-11-12 18:25:51 +0530

    Lines from Dhananjay PC

--- first_prog.py
+++ first_prog.py
@@ -3,3 +3,5 @@ print('Hello world from space :')
 print("hello from world from planet earth")
 # these lines been added from the online github 
 print('Hello world form Git hub world')
+# these lines been added from the ground computer 
+print('Hello Git form Dhananjay PC')
