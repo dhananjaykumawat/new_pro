6f318ede613dc1d97166974df0ca9e14b19bfafe
Dhananjay <44107622+dhananjaykumawat@users.noreply.github.com>
2020-11-12 18:23:33 +0530

    Update first_prog.py

--- first_prog.py
+++ first_prog.py
@@ -1,4 +1,5 @@
 print('Hello World')
 print('Hello world from space :')
 print("hello from world from planet earth")
-
+# these lines been added from the online github 
+print('Hello world form Git hub world')
